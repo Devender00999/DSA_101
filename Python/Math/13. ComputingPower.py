e2c7238f3afaf2ad2ab4d7185b072488a9a6185e
Devender Kumar <devender00999@gmail.com>
2024-05-06 20:00:47 +0530

    added power of n

---
+++ Python/Math/13. ComputingPower.py
@@ -0,0 +1,28 @@
+# computing power of a number
+# ex. 2, 2 = 4; 4,2 = 16
+
+# T(n): O(power of number)
+# S(n): O(1)
+def computePower(x, n):
+   if (x == 0): return 0
+   if (n == 1): return 1
+   res = 1
+   for i in range(0, n):
+      res *= x
+   return res
+
+# idea: x^2 = x * x
+# x^3 = x^2 * x
+# x^5 = x^2 * x^2 * x
+
+# T(n): Theta(log(n))
+# S(n): Theta(log(n))
+def computePowerV2(x, n):
+   if (x == 0): return 0
+   if (n == 0): return 1
+   res = computePowerV2(x, n//2)
+   if (n % 2 == 0): return res * res
+   else: return res * res * x
+
+
+print(computePowerV2(100, 1))
--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 T(n): Time Complexity
 S(n) or AS(n): Auxiliary Space Complexity
+
+Theta: θ
+Oh: O
+Omega: Ω
