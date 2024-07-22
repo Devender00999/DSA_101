64d37fbbfc32da04ce8ed440ddd10321ba1fa95f
Devender Kumar <devender00999@gmail.com>
2024-07-22 20:28:11 +0530

    Added explaination in powerset with time complexity

--- Python/Bitwise Operators/11. PowerSet.py
+++ Python/Bitwise Operators/11. PowerSet.py
@@ -3,14 +3,26 @@
 # o/p: a, b, ab, c, ac, bc, abc
 
 # idea is to use bitwise combination to print all powerset
+#   cba
+# 0 000 
+# 1 001 
+# 2 010
+# 3 011
+# 4 100
+# 5 101
+# 6 110
+# 7 111
 
+# T(n): O(n*2^n)
+# S(n): O(1)
 def printPowerSet(string: str):
    strLen = len(string)
-   power = 2 << strLen - 1
-   for i in range(0, power):
+   power = 1 << strLen
+   for i in range(power):
       for j in range(0, strLen):
-         if i & (1 << j) != 0:
+         if (i & (1 << j) != 0):
             print(string[j], end="")
       print()
 
-printPowerSet('abc')
+
+printPowerSet('ab')
