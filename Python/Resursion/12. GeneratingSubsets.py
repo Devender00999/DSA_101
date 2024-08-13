eb2884d2f882edd94359002f353a2b137217727c
Devender Kumar <devender00999@gmail.com>
2024-08-13 21:08:38 +0530

    Added powerset in generating subset

--- Python/Resursion/12. GeneratingSubsets.py
+++ Python/Resursion/12. GeneratingSubsets.py
@@ -5,14 +5,38 @@
 # i/p: ABC
 # o/p: "", "A", "B", "C", "AB", "AC", "BC", "ABC"
 
+# idea is to start with empty string and use recursion to print each value by include it once and skip it once
+#            ""
+#        /        \
+#       a          ""            a
+#     /   \       /   \ 
+#    ab    a     b     ""        b
+#   /  \   / \  /  \  /  \ 
+#  abc ab ac  a bc  b c   ""     c
+ 
+
 # Recurrence Relation: T(n) = T(n + 1) + T(n + 1)
 # T(n): O(2^n)
 # S(n): O(n)
-def generateSubset(s, curr = "", i = 0):
+def generateSubset(s, curr = '', i = 0):
     if (i == len(s)):
         print(curr)
         return
-    generateSubset(s, curr, i + 1 )     
-    generateSubset(s, curr + s[i], i + 1 )     
-
+    generateSubset(s, curr, i + 1)
+    generateSubset(s, curr + s[i], i + 1)
 generateSubset("ABC")
+
+# to return power set of a string as a list
+# Recurrence Relation: T(n) = T(n + 1) + T(n + 1)
+# T(n): O(2^n)
+# S(n): O(n)
+def powerSet(s, curr="", set = [], i = 0):
+    if (i == len(s)):
+        set.append(curr)
+        return
+    powerSet(s, curr,set, i + 1)
+    powerSet(s, curr + s[i], set, i + 1)
+    set.sort()
+    return set
+
+print(powerSet('ABC'))
