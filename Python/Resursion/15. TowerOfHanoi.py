a6a5fdf67efd33514bb7fcaf1d743dae7c2601d9
Devender Kumar <devender00999@gmail.com>
2024-08-16 21:27:49 +0530

    Added tower of hanoi 🥳

---
+++ Python/Resursion/15. TowerOfHanoi.py
@@ -0,0 +1,25 @@
+# to print the instructions to solve tower of hanoi
+
+# i/p: 3
+# o/p:
+#      Move Disk 1 From A to B
+#      Move Disk 2 From A to C
+#      Move Disk 1 From B to C
+#      Move Disk 3 From A to B
+#      Move Disk 1 From C to A
+#      Move Disk 2 From C to B
+#      Move Disk 1 From A to B
+
+# Recurrence Relation: T(n) = 2T(n - 1) + θ(1)
+# T(n): O(2^n)
+# S(n): (n)
+
+
+def towerOfHanoi(n, a, b, c):
+    if n == 0:
+        return
+    towerOfHanoi(n - 1, a, c, b)
+    print("Move Disk", n, "From", a, "to", b)
+    towerOfHanoi(n - 1, c, b, a)
+
+towerOfHanoi(3, 'A', "B", "C")
