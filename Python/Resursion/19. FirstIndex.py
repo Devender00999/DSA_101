6835501df44ad3be289a3a6ca63fa06176e66a33
Devender Kumar <devender00999@gmail.com>
2024-08-20 22:12:15 +0530

    Added firstIndexV2

--- Python/Resursion/19. FirstIndex.py
+++ Python/Resursion/19. FirstIndex.py
@@ -17,4 +17,14 @@ def firstIndex(arr, idx, d):
         return fiisa
 
 
-print(firstIndex([2, 3, 7, 8 ,4, 9, 0, 8], 0, 3))
+# Recurrence Relation: T(n) = T(n - 1) + θ(1)
+# T(n): O(n)
+# S(n): O(n)    
+def firstIndexV2(arr, idx, d):
+    if (idx == len(arr)): return -1
+    if (arr[idx] == d): return idx
+    fiisa = firstIndexV2(arr, idx + 1, d)
+    return fiisa
+
+
+print(firstIndexV2([2, 3, 7, 8 ,4, 9, 0, 8, 0], 0, 0))
