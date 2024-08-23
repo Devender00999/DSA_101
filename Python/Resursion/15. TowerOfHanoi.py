eed429c3b262beb0ea3f1aeb68380a9af4a2ced5
Devender Kumar <devender00999@gmail.com>
2024-08-23 21:53:04 +0530

    Added Tower hanoi GFG Solution

--- Python/Resursion/15. TowerOfHanoi.py
+++ Python/Resursion/15. TowerOfHanoi.py
@@ -20,4 +20,27 @@ def towerOfHanoi(n, a, b, c):
     print("Move Disk", n, "From", a, "to", b)
     towerOfHanoi(n - 1, c, b, a)
 
-towerOfHanoi(3, 'A', "B", "C")
+# to print the instructions to solve tower of hanoi and length of instructions
+# i/p: 3
+# o/p:
+#      Move Disk 1 From A to B
+#      Move Disk 2 From A to C
+#      Move Disk 1 From B to C
+#      Move Disk 3 From A to B
+#      Move Disk 1 From C to A
+#      Move Disk 2 From C to B
+#      Move Disk 1 From A to B
+#      7
+
+# Recurrence Relation: T(n) = 2T(n - 1) + θ(1)
+# T(n): O(2^n)
+# S(n): (n)
+def towerOfHanoiWithCount(n, a, b, c):
+    if n == 0:
+        return 0
+    lsum = towerOfHanoiWithCount(n - 1, a, c, b)
+    print("Move Disk", n, "From", a, "to", b)
+    rsum = towerOfHanoiWithCount(n - 1, c, b, a)
+    return lsum + rsum  + 1
+
+print(towerOfHanoiWithCount(3, 'A', "B", "C"))
