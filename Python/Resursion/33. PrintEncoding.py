3e52ba3c8e1005af70e796efe73fa95fddb7e38f
Devender Kumar <devender00999@gmail.com>
2024-09-02 21:30:58 +0530

    Added new approach for print encoding with analysis

--- Python/Resursion/33. PrintEncoding.py
+++ Python/Resursion/33. PrintEncoding.py
@@ -3,7 +3,16 @@
 # i/p: 123
 # o/p: abc, aw, lc
 
-# 
+# i/p: 993
+# o/p: iic
+
+# i/p: 303
+# o/p: no output
+
+# Recurrence Relation: T(n) = T(n - 1) + T(n - 2)
+# T(n): O(2^n)
+# S(n): O(n)
+
 encode = ["", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 def printEncoding(st, ans): 
     if (len(st) == 0): 
@@ -23,5 +32,21 @@ def printEncoding(st, ans):
         if (int(st[:2]) <= 26):
             printEncoding(st[2:], ans + encode[int(st[:2])])
         
+# Recurrence Relation: T(n) = T(n - 1) + T(n - 2)
+# T(n): O(2^n)
+# S(n): O(n)
+def printEncodingV2(st, ans):
+    if (len(st) == 0):
+        print(ans)
+        return
+    
+    if (st[0] == '0'):
+        return
+    oneChar = int(st[0])
+    twoChar = int(st[:2])
+    printEncoding(st[1:], ans + encode[oneChar])
+    if (twoChar <= 26 and (len(st[:2]) > 1)):
+        printEncoding(st[2:], ans + encode[twoChar])
 
-printEncoding("993", "")
+printEncoding('103','')
+printEncodingV2('103','')
