20223d4ecd842e693ffdb17c99ec770469102766
Devender Kumar <devender00999@gmail.com>
2024-09-16 20:49:45 +0530

    Added 1 indexed josephus problem

--- Python/Resursion/37. JosephusProblem.py
+++ Python/Resursion/37. JosephusProblem.py
@@ -3,9 +3,21 @@
 # Recurrence Relation: T(n) = T(n - 1)
 # T(n): O(n)
 # S(n): O(n)
+# with 0 indexed elements
 def josephusProblem(n, k):
     if (n == 1):
         return 0
     pistol = josephusProblem(n - 1, k)
     return (pistol + k) % n
-print(josephusProblem(8, 3))
+
+# Recurrence Relation: T(n) = T(n - 1)
+# T(n): O(n)
+# S(n): O(n)
+# with index from 1
+def josephus(n, k):
+    if (n == 1):
+        return 1
+    else:
+        return (josephus(n - 1, k) + k-1) % n + 1
+
+print(josephus(3, 2))
