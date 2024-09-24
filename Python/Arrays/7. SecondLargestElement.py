6db915e702666eb675b81dae8a918b1861d45640
Devender Kumar <devender00999@gmail.com>
2024-09-24 20:44:40 +0530

    Fixed Second Largest element efficient method

--- Python/Arrays/7. SecondLargestElement.py
+++ Python/Arrays/7. SecondLargestElement.py
@@ -6,33 +6,33 @@
 # i/p: 20, 10, 20, 4, 100
 # o/p: 20
 
-
-import math
 # T(n): O(n)
 # S(n): O(1)
 def getSecondLargestElement(arr):
-    max = -math.inf
+    max = -1
     for i in range(len(arr)):
         if (arr[i] > max):                  
             max = arr[i]
-    secondMax = 0
+    secondMax = -1
     for i in range(len(arr)):
         if (arr[i] > secondMax and arr[i] != max):
             secondMax = arr[i]
+    
     return secondMax
 
 # print(getSecondLargestElement([4, 10, 20, 4, 23, 23]))
 # T(n): O(n)
 # S(n): O(1)
-def getSecondLargestElementV2(arr):
-    res = -1
-    max = 0
-    for i in range(len(arr)):
-        if (arr[i] > arr[max]):
-            res = max
-            max = i
-        if res == max:
-            res = -1
-    return arr[res]
+def getLargestAndSecondLargestElementV2(arr):
+    second = -1
+    first = arr[0]
+    for i in range(1, len(arr)):
+        if (arr[i] > first):
+            second = first
+            first = arr[i]
+        elif (arr[i] < first):
+            if second == -1 or second < arr[i]:
+                second = arr[i]
+    return [first, second]
 
-print(getSecondLargestElementV2([10, 20, 4, 23, 25]))
+print(getLargestAndSecondLargestElementV2([10, 10]))
