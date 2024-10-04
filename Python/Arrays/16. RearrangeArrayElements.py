0076dc7aae92543f557faf6fccc56460ab5a9984
Devender Kumar <devender00999@gmail.com>
2024-10-04 09:43:15 +0530

    Added efficeint approach for Rearranging array elements

--- Python/Arrays/16. RearrangeArrayElements.py
+++ Python/Arrays/16. RearrangeArrayElements.py
@@ -11,5 +11,20 @@ def rearrangeArray(arr):
         tempArr.append(arr[arr[i]])
     return tempArr
 
-arr = [1, 0, 4, 2, 3]
-print(rearrangeArray(arr))
+# Idea is to store current number and resulting number by encoding so we don't loose the number when we are replacing number
+# arr[i] = arr[i] + (arr[arr[i]] % n) % n
+# T(n): O(n)
+# S(n): O(n)
+def rearrangeArrayV2(arr):
+    n = len(arr)
+    for i in range(n):
+        currentElem = arr[i]
+        newElem = arr[currentElem]
+        arr[i] = currentElem + (newElem % n) * n
+
+    for i in range(n):
+        arr[i] //= n
+
+arr = [4, 0, 2, 1, 3]
+print(rearrangeArrayV2(arr))
+print(arr)
