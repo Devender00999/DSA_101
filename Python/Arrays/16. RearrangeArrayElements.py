c522d6336bc525ada323dfb442b1d73089c82596
Devender Kumar <devender00999@gmail.com>
2024-10-12 20:45:20 +0530

    Added efficient approach for rearrange array elements

--- Python/Arrays/16. RearrangeArrayElements.py
+++ Python/Arrays/16. RearrangeArrayElements.py
@@ -25,6 +25,32 @@ def rearrangeArrayV2(arr):
     for i in range(n):
         arr[i] //= n
 
+# Idea is to encode each element in array to store two values
+# 1st value: original value
+# 2nd value which will come in its place
+# for even places: we will store maximum and what already their
+# for odd places will store minimum and what's already their
+
+# T(n): O(n)
+# S(n): O(1)
+def rearrangeArrayV2(arr):
+    n = len(arr)
+    maxIdx = n - 1
+    minIdx = 0
+    maxElem = arr[n - 1] + 1
+    for i in range(n):
+        if i % 2 == 0:
+            arr[i] = arr[i] + (arr[maxIdx] % maxElem) * maxElem
+            maxIdx -= 1
+        else:
+            arr[i] = arr[i] + (arr[minIdx] % maxElem) * maxElem
+            minIdx += 1
+    for i in range(n):
+        arr[i] //= maxElem
+
+
+
 arr = [4, 0, 2, 1, 3]
+arr = [1, 2, 3, 4]
 print(rearrangeArrayV2(arr))
 print(arr)
