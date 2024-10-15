ec1c993eb434b7d4085cbc8dae4ad9cca64aff20
Devender Kumar <devender00999@gmail.com>
2024-10-15 21:18:14 +0530

    Added efficient method for Rearranging elements

--- Python/Arrays/21. RearrangeArrayAlternatively.py
+++ Python/Arrays/21. RearrangeArrayAlternatively.py
@@ -28,8 +28,29 @@ def reArrangeArrayAlternatively(arr):
         arr[i] = li[i]
     return li
 
+# Idea is to store original value and new value in the same place by encoding it.
+# to encode it we can use arr[i] = arr[i] + (arr[maxIdx] % maxElem) * maxElem
+# maxElem will be maximum element + 1
+# T(n): O(n)
+# S(n): O(1)
+def reArrangeArrayAlternatively(arr):
+    n = len(arr)
+    maxIdx = n - 1
+    minIdx = 0
+    maxElem = arr[maxIdx] + 1
+    for i in range(n):
+        if i % 2 == 0:
+            arr[i] = arr[i] + (arr[maxIdx] % maxElem) * maxElem
+            maxIdx -= 1
+        else: 
+            arr[i] = arr[i] + (arr[minIdx] % maxElem) * maxElem
+            minIdx += 1
+    for i in range(n):
+        arr[i] //= maxElem
 
-arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110]
-# arr = [1, 2, 3, 4, 5, 6]
+
+# arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110]
+arr = [1, 2, 3, 4, 5, 6]
 # arr= [350,863,1902,2328,2854,3915,6800,8955,9877]
 print(reArrangeArrayAlternatively(arr))
+print(arr)
